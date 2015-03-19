059f68286c147f8e0d297da1e6c7b0316ed3be57
JeroenDM <jeroendemaeyer@live.be>
2015-03-19 10:47:54 +0100

    Temp niet meer printen

--- meetfiets_makro/meetfiets_makro.ino
+++ meetfiets_makro/meetfiets_makro.ino
@@ -259,9 +259,7 @@ boolean saveDataToSD() {
     dataFile.print(Ras, 2);
     dataFile.print("\t");
     dataFile.print(Vbike, 2);
-    dataFile.print("\t");
-    dataFile.println(Temp, 1);
-    dataFile.close();
+    dataFile.println();
     }
   else {
     if(serialOutput) Serial.println("Error in opening file on SD card");
