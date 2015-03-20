60c6719399456909e092a1494b66e1f94f5a1169
JeroenDM <jeroendemaeyer@live.be>
2015-03-20 11:40:19 +0100

    Starting to work on callibration code

---
+++ calibration_code/torque_sensor/torque_sensor.ino
@@ -0,0 +1,13 @@
+/* This code reads the torque sensor and sends it to the pc
+   for real time plotting in scilab to ease calibration
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+  
+}
